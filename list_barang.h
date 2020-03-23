af6e22d46dca6a84b19332bdc462f7bb4ddb2726
lovanto <rifkylovanto@gmail.com>
2020-03-23 18:45:00 +0700

    list_barang.h done

--- list_barang.h
+++ list_barang.h
@@ -1,6 +1,35 @@
 #ifndef LIST_BARANG_H_INCLUDED
 #define LIST_BARANG_H_INCLUDED
 
+#include <iostream>
+using namespace std;
 
+#define first(L) L.first
+#define next(P) P->next
+#define info(P) P->info
+
+typedef int infotype;
+typedef struct elmBarang *adrBarang;
+
+struct elmBarang {
+    infotype info;
+    adrBarang next;
+};
+
+struct listBarang {
+    adrBarang first;
+};
+
+adrBarang alokasi(infotype x);
+adrBarang findElm(listBarang L, infotype x);
+void printInfo(listBarang L);
+
+void createList(listBarang &L);
+void insertFirst(listBarang &L, adrBarang P);
+void insertAfter(listBarang &L, adrBarang Prec, adrBarang P);
+void insertLast(listBarang &L, adrBarang P);
+void deleteFirst(listBarang &L, adrBarang &P);
+void deleteLast(listBarang &L, adrBarang &P);
+void deleteAfter(listBarang &L, adrBarang Prec, adrBarang &P);
 
 #endif // LIST_BARANG_H_INCLUDED
