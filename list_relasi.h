e6e5ddcfd916465d58ac0cef91274fb90e9881d8
lovanto <rifkylovanto@gmail.com>
2020-03-23 18:47:53 +0700

    list_relasi.h done

--- list_relasi.h
+++ list_relasi.h
@@ -1,6 +1,40 @@
-#ifndef LIST_RELASI_H_INCLUDED
-#define LIST_RELASI_H_INCLUDED
+#ifndef listRelasi_H_INCLUDED
+#define listRelasi_H_INCLUDED
 
+#include "listRelasi.h"
+#include "list_child.h"
+#include "list_parent.h"
 
+#include <iostream>
+using namespace std;
 
-#endif // LIST_RELASI_H_INCLUDED
+#define next(P) P->next
+#define first(L) L.first
+#define child(P) P->child
+#define parent(P) P->parent
+
+typedef struct elmRelasi *adrRelasi;
+
+struct elmRelasi{
+    adrRelasi next;
+    adrToko toko;
+    adrBarang barang;
+};
+
+struct listRelasi{
+    adrRelasi first;
+};
+
+adrRelasi alokasi( adrBarang P, adrToko C);
+adrRelasi findElm(listRelasi L, adrBarang P, adrToko C);
+void printInfo(listRelasi L);
+
+void createList(listRelasi &L);
+void insertFirst(listRelasi &L, adrRelasi P);
+void insertLast(listRelasi &L, adrRelasi P);
+void insertAfter(adrRelasi Prec, adrRelasi P);
+void deleteFirst(listRelasi &L, adrRelasi &P);
+void deleteLast(listRelasi &L, adrRelasi &P);
+void deleteAfter(adrRelasi Prec, adrRelasi &P);
+
+#endif // listRelasi_H_INCLUDED
